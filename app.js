c1b788d92edd096777368a6c82a83c56d2e81113
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2025-12-17 10:15:11 +0100

    feat: foto

--- app.js
+++ app.js
@@ -3,35 +3,41 @@ import express from "express";
 const app = express();
 const port = 3001;
 
+app.use(express.static("public"));
 const sweets = [
     {
         id: 1,
-        nome: "ciambellone",
+        nome: "Ciambellone",
         descrizione:" pan di spagna e zuchero",
+        img: "/img/ciambellone.jpeg",
         
     },
     {
         id: 2,
         nome: "Cracker Barbabietola",
         descrizione:" cracker fatto con la barbabietola",
+        img: "/img/cracker_barbabietola.jpeg",
         
     },
     {
         id: 3,
         nome: "Pane fritto dolce",
         descrizione:" pane fritto in olio e ricoperto di zucchero  ",
+        img: "/img/pane_fritto_dolce.jpeg",
         
     },
     {
         id: 4,
         nome: "Pasta Barbabietola",
         descrizione:" pasta fatta con la barbabietola",
+        img: "/img/pasta_barbabietola.jpeg",
         
     },
     {
         id: 5,
         nome: "Torta Paesana",
         descrizione:" torta fatta con mandorle e noci",
+        img: "/img/torta_paesana.jpeg",
         
     },
 ];
@@ -50,7 +56,7 @@ app.get("/bacheca", (req, res) => {
     console.log(numElementi)
 });
 
-app.use(express.static("public"));
+
 
 app.listen(port, function () {
     console.log("funziona")
